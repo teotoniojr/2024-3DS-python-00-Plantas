c19ae28e891e9663f79613e16f8d16717fdcc90d
Teotonio José de Souza Junior <102707827+teotoniojr@users.noreply.github.com>
2024-09-13 10:40:17 -0300

    Add files via upload

--- modelos/aluno.py
+++ modelos/aluno.py
@@ -6,15 +6,29 @@ class Planta:
         self.nome_cientifico = nome_cientifico
         self.genero = genero
         self.grupo = grupo
-        self.extinta = False
+        self._extinta = False
         Planta.plantas.append(self)
         
     def __str__(self):
         return f'{self.nome} | {self.nome_cientifico} | {self.genero} | {self.grupo} | {self.extinta}'
     
     def listar_plantas():
+        print('''
+              
+                █▀█ █░░ ▄▀█ █▄░█ ▀█▀ ▄▀█ █▀   █▀▄ ▄▀█   █▀▄▀█ █ █▄░█ █░█ ▄▀█   █▀▀ ▄▀█ █▀ ▄▀█
+                █▀▀ █▄▄ █▀█ █░▀█ ░█░ █▀█ ▄█   █▄▀ █▀█   █░▀░█ █ █░▀█ █▀█ █▀█   █▄▄ █▀█ ▄█ █▀█
+                
+        ''')
+        print(f'{"Nome da planta".ljust(22)} | {"Nome científico".ljust(22)} | {"Gênero".ljust(22)} | {"Grupo".ljust(22)} | {"Extinto"}')
+        print('---------------------------------------------------------------------------------------------------------')
         for planta in Planta.plantas:
-            print(f'{planta.nome} | {planta.nome_cientifico} | {planta.genero} | {planta.grupo} | {planta.extinta}')
+            print(f'{planta.nome.ljust(22)} | {planta.nome_cientifico.ljust(22)} | {planta.genero.ljust(22)} | {planta.grupo.ljust(22)} | {planta.extinta}')
+            
+        print('---------------------------------------------------------------------------------------------------------') 
+        
+    @property
+    def extinta(self):
+        return '⊘ Extinta' if self._extinta else '❍ Não extinta'
 
 planta_maracuja = Planta('Maracuja', 'Passiflora edulis', 'Passiflora', 'Angiosperma')
 planta_jabuticaba = Planta('Jabuticaba', 'Plinia cauliflora', 'Plinia', 'Angiosperma')
