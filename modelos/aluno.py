4905c7f236542ac249c6c3f71e6e31d0dac1f32f
Teotonio José de Souza Junior <102707827+teotoniojr@users.noreply.github.com>
2024-09-13 07:46:52 -0300

    Add files via upload

--- modelos/aluno.py
+++ modelos/aluno.py
@@ -1,19 +1,23 @@
-class Plantas:
+class Planta:
+    plantas = []
+
     def __init__(self, nome, nome_cientifico, genero, grupo):
         self.nome = nome
         self.nome_cientifico = nome_cientifico
         self.genero = genero
         self.grupo = grupo
         self.extinta = False
+        Planta.plantas.append(self)
         
     def __str__(self):
         return f'{self.nome} | {self.nome_cientifico} | {self.genero} | {self.grupo} | {self.extinta}'
+    
+    def listar_plantas():
+        for planta in Planta.plantas:
+            print(f'{planta.nome} | {planta.nome_cientifico} | {planta.genero} | {planta.grupo} | {planta.extinta}')
 
-planta_maracuja = Plantas('Maracuja', 'Passiflora edulis', 'Passiflora', 'Angiosperma')
-planta_jabuticaba = Plantas('Jabuticaba', 'Plinia cauliflora', 'Plinia', 'Angiosperma')
-planta_araucaria = Plantas('Pinheiro','Araucaria angustifolia', 'Araucaria', 'gimnosperma')
+planta_maracuja = Planta('Maracuja', 'Passiflora edulis', 'Passiflora', 'Angiosperma')
+planta_jabuticaba = Planta('Jabuticaba', 'Plinia cauliflora', 'Plinia', 'Angiosperma')
+planta_araucaria = Planta('Pinheiro','Araucaria angustifolia', 'Araucaria', 'gimnosperma')
 
-plantas = [planta_maracuja, planta_jabuticaba, planta_araucaria]
-
-print(planta_araucaria)
-print(planta_maracuja)
+Planta.listar_plantas()
